f495145c402a1553b78c2ad2724355ec03aa551e
Blake Smith <suicideking2525@gmail.com>
2019-09-11 17:49:12 -0500

    Add project files.

---
+++ MockAssesment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssesment
+{
+    class Program
+    {
+        Random random = new Random();
+        static int[] numList = new int [100];
+        static int p = 1;
+        static int max = numList.Length -1;
+        static int min = 0;
+        static bool correct = false;
+
+
+        //Number Guessing Game
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < numList.Length; i++)
+            {
+                numList[i] = p;
+                p++;
+                Console.WriteLine(numList[i]);
+            }
+
+
+            Console.WriteLine("Welcom to the Wumbo Number Gueesser!");
+            Console.ReadKey();
+            Console.WriteLine("Have you got a number in mind? \nLemme try to guess it");
+
+            string choice = "";
+            while (choice != "0" || !correct)
+            {
+                correct = true;
+
+                if (choice == "1")
+                {
+
+                }
+                if (choice == "2")
+                {
+
+                }
+                if (choice == "3")
+                {
+
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
