6c8a1e99ba3e9d4704d4f9172c19c04bc7a43151
Blake Smith <suicideking2525@gmail.com>
2019-09-13 12:52:25 -0500

    Finsihed the number guessing game and added ano cheating function to min greater than max

--- MockAssesment/Program.cs
+++ MockAssesment/Program.cs
@@ -8,9 +8,9 @@ namespace MockAssesment
 {
     class Program
     {
-        Random random = new Random();
+        static Random random = new Random();
         static int[] numList = new int [100];
-        static int p = 1;
+       
         static int max = numList.Length -1;
         static int min = 0;
         static bool correct = false;
@@ -19,6 +19,12 @@ namespace MockAssesment
         //Number Guessing Game
         static void Main(string[] args)
         {
+            int p = 1;
+
+
+            Console.WriteLine("Welcome to the Wumbo Number Gueesser!");
+            Console.ReadKey();
+
             for (int i = 0; i < numList.Length; i++)
             {
                 numList[i] = p;
@@ -26,32 +32,72 @@ namespace MockAssesment
                 Console.WriteLine(numList[i]);
             }
 
-
-            Console.WriteLine("Welcom to the Wumbo Number Gueesser!");
             Console.ReadKey();
             Console.WriteLine("Have you got a number in mind? \nLemme try to guess it");
+            Console.ReadKey();
 
             string choice = "";
+
+            
+
             while (choice != "0" || !correct)
             {
                 correct = true;
+                if (min > max || max < min)
+                {
+                    Console.WriteLine("You have commited a crime in the world of Wumbo... Begone!");
+                    break;
+                }
+                p = Guess(numList[min], numList[max]);
+                Console.WriteLine("Is this your number? \n" + p + "\n1:Yes. 2:Guess Higher. 3: Guess Lower.");
+                choice = Console.ReadLine();
 
                 if (choice == "1")
                 {
-
+                    Console.WriteLine("YES!!!!! The Wumbo Number Generator has done it again!! Thank you for" +
+                        " playing!!" + " Bye now!");
+                    break;
                 }
-                if (choice == "2")
+                else if (choice == "2")
                 {
+                    Console.WriteLine("I must up the number of Wumbo to reach the number... " +
+                        "Here goes nothing...");
+                    correct = false;
+                    GuessHigher(p);
 
                 }
-                if (choice == "3")
+                else if (choice == "3")
                 {
-
+                    Console.WriteLine("Too much Wumbo has been used here... sorry sir/madame... " +
+                        "lemme try less.");
+                    correct = false;
+                    GuessLower(p);
                 }
+                
             }
-
 
             Console.ReadKey();
         }
+
+        public static int Guess(int _min, int _max)
+        {
+            int choice = Program.random.Next(_min, _max);
+            return choice;
+        }
+
+        public static int GuessHigher(int _min)
+        {
+            min = _min;
+            return min;
+        }
+        
+        public static int GuessLower(int _max)
+        {
+            max = _max - 1;
+
+            return max;
+        }
+        
+
     }
 }
